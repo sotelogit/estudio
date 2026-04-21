f704bfc9653eb539021c71c6b563a5ba908e745c
sotelo marcelo <wn.sotelomarceloariel@gmail.com>
2026-04-20 21:00:25 -0300

    algunos cambios

--- archivos visual code/practicapython/None_menor.py
+++ archivos visual code/practicapython/None_menor.py
@@ -1,7 +1,5 @@
 menor = None
-print('Antes:', menor)
 for valor in [3, 41, 12, 9, 74, 15,2]:
   if menor is None or valor < menor:
      menor = valor
-     print('Bucle:', valor, menor)
-print('Menor:', menor)
+print('El menor es:', menor)
